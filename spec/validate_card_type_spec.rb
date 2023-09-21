f0056ebe8599a52ef79e2077ca4e2a86bcbc7fc8
priya-sodhi <priya.sodhi@xero.com>
2023-09-21 22:32:21 +1200

    [CARD-3] Add cases for remaining card types

--- spec/validate_card_type_spec.rb
+++ spec/validate_card_type_spec.rb
@@ -11,5 +11,46 @@ RSpec.describe "CardTypeValidator" do
                 expect(validate_card_type("6014100011112222")).to eq("Fly Buys Black")
             end
         end
+
+        context "Fly Buys Red Card" do
+            it "validates a valid card with length 16" do
+                expect(validate_card_type("6014352700000140")).to eq("Fly Buys Red")
+            end
+
+            it "validates a valid card starting with 6014352" do
+                expect(validate_card_type("6014352700000140")).to eq("Fly Buys Red")
+            end
+        end
+
+        context "Fly Buys Green Card" do
+            it "validates a valid card with length 16" do
+                expect(validate_card_type("6014355526000020")).to eq("Fly Buys Green")
+            end
+
+            it "validates a valid card starting with 6014355526 or 6014355529" do
+                expect(validate_card_type("6014355526000020")).to eq("Fly Buys Green")
+                expect(validate_card_type("6014355529000020")).to eq("Fly Buys Green")
+            end
+        end
+
+        context "Fly Buys Blue Card" do
+            it "validates a valid card with length 16" do
+                expect(validate_card_type("6014800011112222")).to eq("Fly Buys Blue")
+            end
+
+            it "validates a valid card starting with 6014" do
+                expect(validate_card_type("6014709045001234")).to eq("Fly Buys Blue")
+            end
+        end
+
+        context "Unknown Card" do
+            it "rejects an invalid card number with length not equals to 16" do
+                expect(validate_card_type("6014100011112222111111")).to eq("Unknown")
+            end
+
+            it "rejects an invalid card number not starting with 6104" do
+                expect(validate_card_type("50141000111122221")).to eq("Unknown")
+            end
+        end
     end
 end
--- validate_card_type.rb
+++ validate_card_type.rb
@@ -2,6 +2,12 @@ def validate_card_type(card_number)
     case card_number
     when /^60141\d{11,12}$/
         card_type = "Fly Buys Black"
+    when /^6014352\d{9}$/
+        card_type = 'Fly Buys Red'
+    when /^601435552[69]\d{6}$/
+        card_type = 'Fly Buys Green'
+    when /^6014\d{12}$/
+        card_type = 'Fly Buys Blue'
     else
         card_type = "Unknown"
     end
