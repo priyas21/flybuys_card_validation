83a7bfcb75bb0bc69c5ca29604a44d3e5b999539
priya-sodhi <priya.sodhi@xero.com>
2023-09-21 22:49:11 +1200

    [CARD-4] Add test to validate card number

---
+++ spec/validate_card_number_spec.rb
@@ -0,0 +1,18 @@
+RSpec.describe "CardNumberValidator" do
+    describe "#card_number_valid?" do
+        valid_card_number = "6014355510000028"
+        invalid_card_number = "6014365510000028"
+
+        context "when validating valid card number" do
+            it "returns valid" do
+                expect(validate_card_number(valid_card_number)).to eq("valid")
+            end
+        end
+        
+        context "when validating an invalid card number" do
+            it "returns invalid" do
+                expect(validate_card_number(invalid_card_number)).to eq("invalid")
+            end
+        end
+    end
+end
