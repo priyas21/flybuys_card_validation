576da4e4e1fe758a2a1d592ad3b1060145c5668b
priya-sodhi <priya.sodhi@xero.com>
2023-09-22 00:09:24 +1200

    Add test to display the card in a defined format

---
+++ spec/display_card_validity_with_type_spec.rb
@@ -0,0 +1,18 @@
+require "./display_card_validity_with_type"
+require "./validate_card_type"
+require "./validate_card_number"
+
+RSpec.describe "Print Card in defined format" do
+    describe "#display_card_validity_with_type" do
+        card_number = "60141016700078611"
+
+        it "displays the validity and type of card in the format 'TYPE: NUMBERS (VALIDITY)'" do
+
+            allow(self).to receive(:validate_card_type).and_return("Fly Buys Black")
+            allow(self).to receive(:validate_card_number).and_return("valid")
+
+            # Check if the output matches the expected format
+            expect(display_card_validity_with_type(card_number)).to eq("Fly Buys Black: #{card_number} (valid)")
+        end
+    end
+end
